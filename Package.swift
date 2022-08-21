4c44699bfade30084ff154767212eec1cc3ddd3a
Marco Eidinger <eidingermarco@gmail.com>
2022-08-21 10:41:50 -0700

    feat: 🎸 use nicklockwood/SwiftFormat@0.49.15

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "swiftformat",
-            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.49.14/swiftformat.artifactbundle.zip",
-            checksum: "30e7fccc5a74875018e1556fa570eae4baf87c40976c7d6fa21cf0d282f1c340"
+            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.49.15/swiftformat.artifactbundle.zip",
+            checksum: "af3ec3d06313c6cf013c6949faaabcf5f3d80f0f7409360b158190e1ba5c291b"
         ),
         .plugin(name: "SwiftFormat",
                 capability: .command(
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Add the package to your dependencies.
 ```swift
 dependencies: [
     // ...
-    .package(url: "https://github.com/MarcoEidinger/SwiftFormatPlugin", from: "0.49.14"),
+    .package(url: "https://github.com/MarcoEidinger/SwiftFormatPlugin", from: "0.49.15"),
 ]
 ```
 
