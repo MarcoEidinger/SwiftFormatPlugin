db39e6091a1981c63eae2577c701544628cd0e86
Marco Eidinger <eidingermarco@gmail.com>
2022-10-02 05:37:18 -0700

    feat: 🎸 use nicklockwood/SwiftFormat@0.49.18

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "swiftformat",
-            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.49.17/swiftformat.artifactbundle.zip",
-            checksum: "bb4d8b2105b2e81fbc660f5c2e0ae7593581263ba1867eea2242bf705cf299ae"
+            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.49.18/swiftformat.artifactbundle.zip",
+            checksum: "47e8ecd01fca0ea3c21029a0ea53b3dff4eb712e6164c5c796e4c74c7facf073"
 
 
         ),
